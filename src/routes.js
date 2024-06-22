1cff8b77ad7cc639a791f341fb18b006d7cb4282
LucasMazetto-94 <lucas.brabes@terra.com.br>
2024-06-22 19:25:41 -0300

    criação do diretorio upload

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const arboController = require('./controllers/arboControllers');
 
 router.get('/clientes', arboController.buscarTodos);
-router.get('/produtos', arboController.buscarTodosProdutos); 
+router.get('/produtos', arboController.buscarTodosProdutos);
+router.get('/pedidos', arboController.buscarTodosPedidos); 
+router.post('/cadastro_cliente', arboController.cadastrarCliente);
+
 
 module.exports = router;
