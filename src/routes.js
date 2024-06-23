cee1e44714aedcc077bfdd6b7ec80efe48ee45f5
LucasMazetto-94 <lucas.brabes@terra.com.br>
2024-06-22 21:40:18 -0300

    Finalização do multer

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-
 const arboController = require('./controllers/arboControllers');
+const upload = require('../src/uploadConfig')
 
 router.get('/clientes', arboController.buscarTodos);
 router.get('/produtos', arboController.buscarTodosProdutos);
 router.get('/pedidos', arboController.buscarTodosPedidos); 
 router.post('/cadastro_cliente', arboController.cadastrarCliente);
+router.post('/cadastro_produtos', upload.single('photo'), arboController.criarProduto);
 
 
 module.exports = router;
